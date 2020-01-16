9039932414084ef574db922d06e77dafb8a1065e
dakota1599 <dakotashapiro15@gmail.com>
2020-01-16 13:06:06 -0500

    Change designed in the footer to developed.

--- views/partials/footer.partial.php
+++ views/partials/footer.partial.php
@@ -4,7 +4,7 @@
 
 <div id="footer">
     <p class="fg-tertiary" style="margin-top: .5em;">&copy; <?= date('Y')?> Writing Mentor</p>
-    <p class="fg-tertiary">Website designed by <a id="footerLink" class="transition" target="_blank" href="https://dakotashapiro.info" title="Dakota's Website">Dakota Shapiro</a></p>
+    <p class="fg-tertiary">Website developed by <a id="footerLink" class="transition" target="_blank" href="https://dakotashapiro.info" title="Dakota's Website">Dakota Shapiro</a></p>
 </div>
 <script>
   AOS.init();
